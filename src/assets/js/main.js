ecefd17690f31a5c5f82fda350e2d0ba1355b34a
Henrique <69041032+ApiLogik@users.noreply.github.com>
2021-05-04 15:15:25 -0300

    Add files via upload

---
+++ src/assets/js/main.js
@@ -0,0 +1,25 @@
+import Mask from '../../modules/phonemask.js';
+
+//-----------Funções principais----------------
+const loadForm = () => {
+	const inputTel = document.querySelector('#input-tel');
+	
+	const loadMask = () => {
+		//Novo objeto Mask - parâmetro: o campo p/ tel do formulário;
+		const phoneMask = new Mask(inputTel);
+		phoneMask.mask();
+	}
+
+	const showMsg = e => {
+		document.querySelector('form p').innerHTML = '';
+		document.querySelector('form p').innerHTML = 'O número está no formato aceito.';
+		e.preventDefault();
+	}
+
+	document.querySelector('form').addEventListener('submit', e => showMsg(e));
+
+	loadMask();
+	
+}//loadForm
+
+loadForm();
